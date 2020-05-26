73d34e8016b7e390f4ae09083df332d471cd2f37
Oswaldo Gallo Romero <oswaldo.gallor@gmail.com>
2020-05-26 11:21:23 -0500

    HTTP Request

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../providers/user.service';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+    users: any[] = [];
 
+    constructor(
+        public userService: UserService
+    ) {
+    }
+
+    ngOnInit() {
+        this.userService.getUsers()
+            .subscribe(
+                (data) => { // Success
+                    this.users = data['results'];
+                },
+                (error) => {
+                    console.error(error);
+                }
+            );
+    }
 }
